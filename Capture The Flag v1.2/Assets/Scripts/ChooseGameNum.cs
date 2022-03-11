7823c3d2a16f6b2526d033e07ec58cae202f5964
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-03-10 18:00:32 -0800

    v1.2

---
+++ Capture The Flag v1.2/Assets/Scripts/ChooseGameNum.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static GameVariables;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChooseGameNum : MonoBehaviour
+{
+    public Text writtenNum;
+
+    void Update()
+    {
+        writtenNum.text = gameNum.ToString();
+    }
+
+    public void IncreaseGameNum()
+    {
+        gameNum += 1;
+        Debug.Log(gameNum);
+
+    }
+
+    public void DecreaseGameNum()
+    {
+        if (gameNum > 1)
+        {
+            gameNum -= 1;
+            Debug.Log(gameNum);
+        }
+
+
+    }
+
+    public void StartGame()
+    {
+        if (twoPlayers == true)
+        {
+            SceneManager.LoadScene(2);
+        }
+        else
+        {
+            SceneManager.LoadScene(3);
+
+        }
+    }
+}
---
+++ Capture The Flag v1.2/Assets/Scripts/GameVariables.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.SceneManagement;
+
+public static class GameVariables
+{
+    public static bool twoPlayers = false;
+    public static int blueScore = 0;
+    public static int redScore = 0;
+    public static int gameNum = 3;
+    public static int currentGame;
+
+
+    public static void Restart()
+    {
+
+        SceneManager.LoadScene(4);
+
+    }
+}
