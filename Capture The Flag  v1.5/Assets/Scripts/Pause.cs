2e22b5d4ad98d64360b197f464c1fd854f2cad8b
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-03-10 18:11:08 -0800

    v1.5

---
+++ Capture The Flag  v1.5/Assets/Scripts/Pause.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static GameVariables;
+
+public class Pause : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    public Animator pauseAnim;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            if (pause == false)
+            {
+                pauseMenu.SetActive(true);
+
+                pause = true;
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+
+                pause = false;
+            }
+                Debug.Log(pause);
+        }
+    }
+    
+}
---
+++ Capture The Flag  v1.5/Assets/Scripts/PickMap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickMap : MonoBehaviour
+{
+    public GameObject[] maps;
+    public GameObject template;
+
+    void Start()
+    {
+        ChooseMap();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("space") && Input.GetKeyDown("z"))
+        {
+            ChooseMap();
+        }
+
+    }
+
+    public void ChooseMap()
+    {
+        for (int i = 0; i < maps.Length; i++)
+        {
+            maps[i].SetActive(false);
+        }
+
+        template.SetActive(false);
+        int randMap = Random.Range(0, maps.Length);
+        Debug.Log(randMap);
+        maps[randMap].SetActive(true);
+    }
+
+}
